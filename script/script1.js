f5a8c49e201c4b29180fb6fec49057c12c901789
Mike9905 <miguel.galvan@uabc.edu.mx>
2025-02-23 21:31:00 -0700

    Tercera version/Pagina de proyectos agregada/Inicio alineado

---
+++ script/script1.js
@@ -0,0 +1,33 @@
+
+
+// keyword variableName = value;
+// var, let, const
+
+//String
+var firstName = "Samantha";
+var lastName = "Jimenez";
+
+firstName = "Fernanda";
+console. log (firstName);
+
+//concatenation
+console.log("Nombre: " +firstName +" " +lastName) 
+//numerical
+let myAge=99;
+let myHeight=100;
+
+console. log(myAge, myHeight);
+
+//boolean (true/false)
+let isStudent = true;
+
+//math operations
+let num1=10;
+let num2=12;
+
+document.write("El resultado es = " + (num1+num2))
+console.log("El resultado es = " + (num1-num2))
+console.log("El resultado es = " + (num1*num2))
+console.log("El resultado es = " + (num1/num2).toFixed(3))
+
+//alert("Esto es un mensaje de alerta");
